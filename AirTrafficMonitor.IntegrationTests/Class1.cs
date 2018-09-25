a5819a0f9dfcf02d60a87cf340ca176cc6554840
grottrup <grottrup@protonmail.com>
2018-09-25 08:58:58 +0200

    Setup test fixture

--- AirTrafficMonitor.IntegrationTests/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace AirTrafficMonitor.IntegrationTests
-{
-    public class Class1
-    {
-    }
-}
---
+++ AirTrafficMonitor.IntegrationTests/IntegrationTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace AirTrafficMonitor.Tests
+{
+    [TestFixture]
+    public class IntegrationTests
+    {
+        [TestCase(TestName = "My first requirement")]
+        public void Test1()
+        {
+
+        }
+    }
+}
