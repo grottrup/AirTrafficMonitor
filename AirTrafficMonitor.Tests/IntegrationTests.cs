dd44bef460fa86d0005d211cf603321b27ee9907
grottrup <grottrup@protonmail.com>
2018-09-27 18:14:27 +0200

    Give up on using Standard and create Framework libs instead

---
+++ AirTrafficMonitor.Tests/IntegrationTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace AirTrafficMonitor.Tests
+{
+    [TestFixture]
+    public class IntegrationTests
+    {
+        [TestCase(TestName = "The system shall monitor one (1) airspace.")]
+        public void Test1()
+        {
+
+        }
+
+        [TestCase(TestName = "The system shall render all tracks currently in the monitored airspace.")]
+        public void Test2()
+        {
+
+        }
+
+        //TODO what do we want to render it to?
+        [TestCase(TestName = "Rendition of the tracks in the airspace shall be [either to a file or to the console]???")]
+        public void Test3()
+        {
+
+        }
+
+        [TestCase(TestName = "The monitored airspace has a boundary")]
+        public void Test4()
+        {
+
+        }
+
+        [TestCase(TestName = "The monitored airspace has a boundary")]
+        public void Test5()
+        {
+
+        }
+    }
+}
