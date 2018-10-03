547e942afa4efc7aae305a40606210f4ea792388
Michelle True Sørensen <farcait@gmail.com>
2018-10-03 16:30:44 +0200

    Creates new CollisionEvent branch to match with master pull

--- Source/AirTrafficMonitor/AirTrafficMonitor/SeparationHandler.cs
+++ Source/AirTrafficMonitor/AirTrafficMonitor/SeparationHandler.cs
@@ -6,23 +6,46 @@ namespace AirTrafficMonitor
 {
     public class SeparationHandler : ISeperationHandler
     {
-
-        public event EventHandler SeparationEvent;
-
-        public int VertDist = 300;
-        public int HoriDist = 5000;
+        //private SeparationData _separationData;
+        //public event EventHandler SeparationAlert;
         //public SubscribePls = new AirTrafficMonitor.Observer;
-        //public void Update(FlightRecord update)
+        //public void Update(AirTrafficRecord update)
         //{
-          
-           
         //}
-
+        public bool State = false;
+        public int buf = 0;
         public void DetectCollision(List<FlightTrack> tracks)
         {
             foreach (var flights in tracks)
             {
-                
+                for (int i = 0; i < tracks.Count - 1; i++)
+                {
+                    if (tracks[i]._records[tracks[i]._records.Count - 1].Timestamp == tracks[i + 1]._records[tracks[i + 1]._records.Count - 1].Timestamp)
+                    {   //Beregn hori dist
+                        var HorizontialDistance = Math.Abs(Math.Sqrt(
+                            Math.Pow(tracks[i]._records[tracks[i]._records.Count - 1].Position.X - tracks[i + 1]._records[tracks[i + 1]._records.Count - 1].Position.X, 2) +
+                            Math.Pow(tracks[i]._records[tracks[i]._records.Count - 1].Position.Y - tracks[i]._records[tracks[i]._records.Count - 1].Position.Y, 2)));
+
+                        var VerticalDistance =
+                            Math.Abs(tracks[i]._records[tracks[i]._records.Count - 1].Altitude - tracks[i + 1]._records[tracks[i + 1]._records.Count - 1].Altitude);
+
+                        if (VerticalDistance < 300 && HorizontialDistance < 5000)
+                        {
+                            Console.WriteLine("JUHUUU eller AAH NEJ");
+
+                            //handler
+
+                            //tate = true;
+
+                            //tracks > _record = fly.tag > ATRecord > Data
+
+                        }
+                        else
+                            continue;
+                    }
+                    else
+                        continue;
+                }
             }
         }
     }
