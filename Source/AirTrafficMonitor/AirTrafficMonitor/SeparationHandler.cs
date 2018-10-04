af497160306f6411872f7a224888c0c7c821991c
grottrup <grottrup@protonmail.com>
2018-10-04 12:42:41 +0200

    uncomment seperation handler

--- Source/AirTrafficMonitor/AirTrafficMonitor/SeparationHandler.cs
+++ Source/AirTrafficMonitor/AirTrafficMonitor/SeparationHandler.cs
@@ -12,98 +12,103 @@ namespace AirTrafficMonitor
         public List<FlightInCollision> _FlightInCollisionDetected;
         public ILogger _Logger;
 
-        public SeparationHandler(FlightInCollision flightInCollisionData, ILogger logger)
-        {
-            _flightInCollisionData = flightInCollisionData;
-            _FlightInCollisionDetected = new List<FlightInCollision>();
-            _Logger = logger;
-        }
-
         public void DetectCollision(List<FlightTrack> tracks)
         {
-            
-                if (TagState(tracks))
-                 {
-                     if (TimeState(tracks))
-                     {
-                         if (Verticaldistance(tracks) < 300 && Horizontaldistance(tracks) < 5000)
-                         {
-                            
-                                 for (int i = 0; i < tracks.Count - 1; i++)
-                                 {
-
-                                     _flightInCollisionData.Tag1 = tracks[i]._records[tracks[i]._records.Count - 1].Tag;
-                                     _flightInCollisionData.Tag2 =
-                                         tracks[i + 1]._records[tracks[i + 1]._records.Count - 1].Tag;
-                                     _flightInCollisionData.TimeStamp =
-                                         tracks[i]._records[tracks[i]._records.Count - 1].Timestamp;
-
-                                     _FlightInCollisionDetected.Add(_flightInCollisionData);
-
-                                     _Logger.DataLog(_flightInCollisionData.Tag1, _flightInCollisionData.Tag2, _flightInCollisionData.TimeStamp);
-                                     //_Logger.ConsoleLog(_flightInCollisionData.Tag1, _flightInCollisionData.Tag2, _flightInCollisionData.TimeStamp);
-                                 }
-
-
-                         }
-                     }
-                 }
+            throw new NotImplementedException();
         }
 
-        public bool TagState(List<FlightTrack> tracks)
-        {
-            if (tracks.Count != 0)
-            {
-                for (int i = 0; i < tracks.Count - 1; i++)
-                {
+        //public SeparationHandler(FlightInCollision flightInCollisionData, ILogger logger)
+        //{
+        //    _flightInCollisionData = flightInCollisionData;
+        //    _FlightInCollisionDetected = new List<FlightInCollision>();
+        //    _Logger = logger;
+        //}
 
-                    return tracks[i]._records[tracks[i]._records.Count - 1].Tag !=
-                           tracks[i + 1]._records[tracks[i + 1]._records.Count - 1].Tag;
-                }
-            }
+        //public void DetectCollision(List<FlightTrack> tracks)
+        //{
 
-            return false;
-        }
-        public bool TimeState(List<FlightTrack> tracks)
-        {
-            if (tracks.Count != 0)
-            { 
-                for (int i = 0; i < tracks.Count-1; i++)
-                {
-                    return tracks[i]._records[tracks[i]._records.Count - 1].Timestamp ==
-                           tracks[i + 1]._records[tracks[i + 1]._records.Count - 1].Timestamp;
-                }
-            }
+        //        if (TagState(tracks))
+        //         {
+        //             if (TimeState(tracks))
+        //             {
+        //                 if (Verticaldistance(tracks) < 300 && Horizontaldistance(tracks) < 5000)
+        //                 {
 
-            return false;
+        //                         for (int i = 0; i < tracks.Count - 1; i++)
+        //                         {
 
-        }
-        public double Horizontaldistance(List<FlightTrack> tracks)
-        {
-            if (tracks.Count != 0)
-            { 
-                for (int i = 0; i < tracks.Count-1; i++)
-                {
-                    return Math.Round(Math.Abs(Math.Pow(tracks[i]._records[tracks[i]._records.Count - 1].Position.X - tracks[i + 1]._records[tracks[i + 1]._records.Count - 1].Position.X, 2) 
-                                               + Math.Pow(tracks[i]._records[tracks[i]._records.Count - 1].Position.Y - tracks[i + 1]._records[tracks[i + 1]._records.Count - 1].Position.Y, 2)));
-                }
-            }
+        //                             _flightInCollisionData.Tag1 = tracks[i]._records[tracks[i]._records.Count - 1].Tag;
+        //                             _flightInCollisionData.Tag2 =
+        //                                 tracks[i + 1]._records[tracks[i + 1]._records.Count - 1].Tag;
+        //                             _flightInCollisionData.TimeStamp =
+        //                                 tracks[i]._records[tracks[i]._records.Count - 1].Timestamp;
 
-            return 0;
-        }
-        public double Verticaldistance(List<FlightTrack> tracks)
-        {
-            if (tracks.Count != 0)
-            { 
-                for (int i = 0; i < tracks.Count-1; i++)
-            
-                {
-                    return Math.Abs(tracks[i]._records[tracks[i]._records.Count - 1].Altitude -
-                                    tracks[i + 1]._records[tracks[i + 1]._records.Count - 1].Altitude);
-                }
-            }
+        //                             _FlightInCollisionDetected.Add(_flightInCollisionData);
 
-            return 0;
-        }
+        //                             _Logger.DataLog(_flightInCollisionData.Tag1, _flightInCollisionData.Tag2, _flightInCollisionData.TimeStamp);
+        //                             //_Logger.ConsoleLog(_flightInCollisionData.Tag1, _flightInCollisionData.Tag2, _flightInCollisionData.TimeStamp);
+        //                         }
+
+
+        //                 }
+        //             }
+        //         }
+        //}
+
+        //public bool TagState(List<FlightTrack> tracks)
+        //{
+        //    if (tracks.Count != 0)
+        //    {
+        //        for (int i = 0; i < tracks.Count - 1; i++)
+        //        {
+
+        //            return tracks[i]._records[tracks[i]._records.Count - 1].Tag !=
+        //                   tracks[i + 1]._records[tracks[i + 1]._records.Count - 1].Tag;
+        //        }
+        //    }
+
+        //    return false;
+        //}
+        //public bool TimeState(List<FlightTrack> tracks)
+        //{
+        //    if (tracks.Count != 0)
+        //    { 
+        //        for (int i = 0; i < tracks.Count-1; i++)
+        //        {
+        //            return tracks[i]._records[tracks[i]._records.Count - 1].Timestamp ==
+        //                   tracks[i + 1]._records[tracks[i + 1]._records.Count - 1].Timestamp;
+        //        }
+        //    }
+
+        //    return false;
+
+        //}
+        //public double Horizontaldistance(List<FlightTrack> tracks)
+        //{
+        //    if (tracks.Count != 0)
+        //    { 
+        //        for (int i = 0; i < tracks.Count-1; i++)
+        //        {
+        //            return Math.Round(Math.Abs(Math.Pow(tracks[i]._records[tracks[i]._records.Count - 1].Position.X - tracks[i + 1]._records[tracks[i + 1]._records.Count - 1].Position.X, 2) 
+        //                                       + Math.Pow(tracks[i]._records[tracks[i]._records.Count - 1].Position.Y - tracks[i + 1]._records[tracks[i + 1]._records.Count - 1].Position.Y, 2)));
+        //        }
+        //    }
+
+        //    return 0;
+        //}
+        //public double Verticaldistance(List<FlightTrack> tracks)
+        //{
+        //    if (tracks.Count != 0)
+        //    { 
+        //        for (int i = 0; i < tracks.Count-1; i++)
+
+        //        {
+        //            return Math.Abs(tracks[i]._records[tracks[i]._records.Count - 1].Altitude -
+        //                            tracks[i + 1]._records[tracks[i + 1]._records.Count - 1].Altitude);
+        //        }
+        //    }
+
+        //    return 0;
+        //}
     }
 }
