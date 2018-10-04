05903d48f2cb4b7457f2469c63304cdecb8b9495
Mike <Mike-mj@hotmail.com>
2018-10-04 09:09:03 +0200

    Separationhandler, and test

---
+++ Source/AirTrafficMonitor/AirTrafficMonitor/FlightInCollision.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace AirTrafficMonitor
+{
+    public class FlightInCollision
+    {
+        public string Tag1 { get; set; }
+        public string Tag2 { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+}
--- Source/AirTrafficMonitor/AirTrafficMonitor/SeparationHandler.cs
+++ Source/AirTrafficMonitor/AirTrafficMonitor/SeparationHandler.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using AirTrafficMonitor.View;
 
 namespace AirTrafficMonitor
@@ -7,23 +8,97 @@ namespace AirTrafficMonitor
     public class SeparationHandler : ISeperationHandler
     {
 
-        public event EventHandler SeparationEvent;
+        public FlightInCollision _flightInCollisionData;
+        public List<FlightInCollision> _FlightInCollisionDetected;
 
-        public int VertDist = 300;
-        public int HoriDist = 5000;
-        //public SubscribePls = new AirTrafficMonitor.Observer;
-        //public void Update(FlightRecord update)
-        //{
-          
-           
-        //}
+        public SeparationHandler(FlightInCollision flightInCollisionData)
+        {
+            _flightInCollisionData = flightInCollisionData;
+            _FlightInCollisionDetected = new List<FlightInCollision>();
+        }
 
         public void DetectCollision(List<FlightTrack> tracks)
         {
-            foreach (var flights in tracks)
+            if (TagState(tracks))
             {
-                
+                if (TimeState(tracks))
+                {
+                    if (Verticaldistance(tracks) < 300 && Horizontaldistance(tracks)< 5000)
+                    {
+                        if (tracks.Count != 0)
+                        { 
+                            for (int i = 0; i < tracks.Count-1; i++)
+                            {
+                                _flightInCollisionData.Tag1 = tracks[i]._records[tracks[i]._records.Count - 1].Tag;
+                                _flightInCollisionData.Tag2 = tracks[i+1]._records[tracks[i+1]._records.Count - 1].Tag;
+                                _flightInCollisionData.TimeStamp = tracks[i]._records[tracks[i]._records.Count - 1].Timestamp;
+
+                                _FlightInCollisionDetected.Add(_flightInCollisionData);
+                                
+                                //Log Event?
+                                //Console Event?
+                             
+                            }
+                        }
+                    }
+                }
             }
+        }
+
+        public bool TagState(List<FlightTrack> tracks)
+        {
+            if (tracks.Count != 0)
+            {
+                for (int i = 0; i < tracks.Count - 1; i++)
+                {
+
+                    return tracks[i]._records[tracks[i]._records.Count - 1].Tag !=
+                           tracks[i + 1]._records[tracks[i + 1]._records.Count - 1].Tag;
+                }
+            }
+
+            return false;
+        }
+        public bool TimeState(List<FlightTrack> tracks)
+        {
+            if (tracks.Count != 0)
+            { 
+                for (int i = 0; i < tracks.Count-1; i++)
+                {
+                    return tracks[i]._records[tracks[i]._records.Count - 1].Timestamp ==
+                           tracks[i + 1]._records[tracks[i + 1]._records.Count - 1].Timestamp;
+                }
+            }
+
+            return false;
+
+        }
+        public double Horizontaldistance(List<FlightTrack> tracks)
+        {
+            if (tracks.Count != 0)
+            { 
+                for (int i = 0; i < tracks.Count-1; i++)
+                {
+                    return Math.Round(Math.Abs(Math.Pow(tracks[i]._records[tracks[i]._records.Count - 1].Position.X - tracks[i + 1]._records[tracks[i + 1]._records.Count - 1].Position.X, 2) 
+                                               + Math.Pow(tracks[i]._records[tracks[i]._records.Count - 1].Position.Y - tracks[i + 1]._records[tracks[i + 1]._records.Count - 1].Position.Y, 2)));
+                }
+            }
+
+            return 0;
+        }
+        public double Verticaldistance(List<FlightTrack> tracks)
+        {
+            if (tracks.Count != 0)
+            { 
+                for (int i = 0; i < tracks.Count-1; i++)
+            
+                {
+                    return Math.Abs(tracks[i]._records[tracks[i]._records.Count - 1].Altitude -
+                                    tracks[i + 1]._records[tracks[i + 1]._records.Count - 1].Altitude);
+                }
+            }
+
+            return 0;
         }
     }
 }
