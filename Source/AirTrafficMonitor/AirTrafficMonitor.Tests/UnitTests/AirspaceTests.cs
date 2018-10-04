95b68234bc125338629d103b8b4b9b77ec417157
grottrup <grottrup@protonmail.com>
2018-10-04 12:21:14 +0200

    Update airspace test

--- Source/AirTrafficMonitor/AirTrafficMonitor.Tests/UnitTests/AirspaceTests.cs
+++ Source/AirTrafficMonitor/AirTrafficMonitor.Tests/UnitTests/AirspaceTests.cs
@@ -21,14 +21,12 @@ namespace AirTrafficMonitor.Tests.UnitTests
             var record3 = factory.CreateRecord("AGJ063;39563;91000;16800;20181001160609975"); //False
             var record4 = factory.CreateRecord("AGJ063;40563;80000;13800;20181001160609975"); //True
 
-            //Create airspace
-            var Space = new Airspace();
+            var airspace = new Airspace();
 
-            Assert.IsTrue(Space.IsWithin(record1.Position, record1.Altitude));
-            Assert.IsFalse(Space.IsWithin(record2.Position, record2.Altitude));
-            Assert.IsFalse(Space.IsWithin(record3.Position, record3.Altitude));
-            Assert.IsTrue(Space.IsWithin(record4.Position, record4.Altitude));
-            //test Airspace.IsWithin()
+            Assert.IsTrue(record1.Position.IsWithin(airspace));
+            Assert.IsFalse(record2.Position.IsWithin(airspace));
+            Assert.IsFalse(record3.Position.IsWithin(airspace));
+            Assert.IsTrue(record4.Position.IsWithin(airspace));
         }
     }
 }
