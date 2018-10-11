a6a044ea2930932630076c69a2064bf0fb726e4d
grottrup <grottrup@protonmail.com>
2018-10-11 02:23:52 +0200

    Still fixing merge error

--- Source/AirTrafficMonitor/AirTrafficMonitor/Observer/FlightObserver.cs
+++ Source/AirTrafficMonitor/AirTrafficMonitor/Observer/FlightObserver.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using AirTrafficMonitor.Observer;
+using AirTrafficMonitor.Util;
 
 namespace AirTrafficMonitor.View
 {
-    public class FlightObserver : Observer.IObserver<FlightRecord> //might be deleted later
+    public class FlightObserver : Observer.IObserver<FlightRecord>
     {
         private readonly List<FlightTrack> _tracks;
         private readonly IView _view;
@@ -24,29 +25,9 @@ namespace AirTrafficMonitor.View
         {
             if (update.Position.IsWithin(_space))
             {
-                SortRecordByTag(update);
+                _tracks.SortRecordByTag(update);
                 _handler.DetectCollision(_tracks);
                 _view.Render(update);
-            }
-        }
-
-        private void SortRecordByTag(FlightRecord update)
-        {
-            if (!_tracks.Any(track => track.Tag == update.Tag))
-            {
-                var newTrack = new FlightTrack(update.Tag);
-                newTrack.Add(update);
-                _tracks.Add(newTrack);
-            }
-            else
-            {
-                foreach (var track in _tracks)
-                {
-                    if (track.Tag == update.Tag)
-                    {
-                        track.Add(update);
-                    }
-                }
             }
         }
     }
