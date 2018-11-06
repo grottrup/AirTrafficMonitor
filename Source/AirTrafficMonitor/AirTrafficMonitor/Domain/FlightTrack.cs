4693d9479f7956cb3476f1fe4936d4d9ab87f740
grottrup <grottrup@protonmail.com>
2018-11-06 11:36:44 +0100

    Add velocity calculation

--- Source/AirTrafficMonitor/AirTrafficMonitor/Domain/FlightTrack.cs
+++ Source/AirTrafficMonitor/AirTrafficMonitor/Domain/FlightTrack.cs
@@ -11,7 +11,7 @@ namespace AirTrafficMonitor.Domain
         public string Tag { get; }
         public DateTime LatestTime { get; private set; }
         public int NavigationCourse { get; private set; }
-        public int Velocity { get; private set; }
+        public double Velocity { get; private set; }
         public Position Position { get; private set; }
 
         public FlightTrack(string tag)
@@ -29,8 +29,30 @@ namespace AirTrafficMonitor.Domain
                 LatestTime = record.Timestamp;
                 Position = record.Position;
                 NavigationCourse = CalculateNavigationCourse();
-                Velocity = 0;
+                Velocity = CalculateVelocity();
             }
+        }
+
+        private double CalculateVelocity()
+        {
+            if (_records.Count == 2)
+            {
+                var lon1 = _records.First().Position.Longitude;
+                var lat1 = _records.First().Position.Latitude;
+                var time1 = _records.First().Timestamp;
+
+                var lon2 = _records.Last().Position.Longitude;
+                var lat2 = _records.Last().Position.Latitude;
+                var time2 = _records.Last().Timestamp;
+
+                int deltaPosition = (int) Math.Sqrt(Math.Pow(lon1 - lon2, 2) + Math.Pow(lon2 - lat2, 2));
+
+                double deltaTime = (time2 - time1).TotalSeconds;
+
+                return deltaPosition / deltaTime;
+            }
+
+            return 0;
         }
 
         private int CalculateNavigationCourse()
@@ -43,6 +65,7 @@ namespace AirTrafficMonitor.Domain
                 int lat2 = _records.Last().Position.Latitude;
                 int deltaLon = Math.Abs(lon1 - lon2);
                 int deltaLat = Math.Abs(lat1 - lat2);
+
                 if (deltaLat > 0)
                 {
                     return (int) (90 - Math.Atan(Math.Sin(deltaLat / deltaLon)) * 180 / Math.PI);
@@ -60,8 +83,7 @@ namespace AirTrafficMonitor.Domain
                     return 0;
                 }
             }
-            return -1;
-            //return (int)Math.Atan2(Math.Sin(deltaLon) * Math.Cos(lat2), Math.Cos(lat1) * Math.Sin(lat2) - Math.Sin(lat1) * Math.Cos(lat2) * Math.Cos(deltaLat));
+            return 0;
         }
 
         public override string ToString()
