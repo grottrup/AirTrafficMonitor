7166fd52c9ac4f0ac18428129551cef10370acbf
grottrup <grottrup@protonmail.com>
2018-11-06 11:50:35 +0100

    Start making velocity test

--- Source/AirTrafficMonitor/AirTrafficMonitor.Tests/FlightTrack_Should.cs
+++ Source/AirTrafficMonitor/AirTrafficMonitor.Tests/FlightTrack_Should.cs
@@ -28,5 +28,19 @@ namespace AirTrafficMonitor.Tests
 
             Assert.That(uut.NavigationCourse, Is.EqualTo(expectedCourse));
         }
+
+        //[TestCase(0, 0, 0, 0, 0)]
+        public void GivenTwoPositionRecords_CalculateVelocity(int lon1, int lat1, int lon2, int lat2, int expectedVelocity)
+        {
+            var uut = new FlightTrack("test flight");
+
+            var record1 = new FlightRecord() { Position = new Position(lat1, lon1, 0) };
+            var record2 = new FlightRecord() { Position = new Position(lat2, lon2, 0) };
+
+            uut.Update(record1);
+            uut.Update(record2);
+
+            Assert.That(uut.Velocity, Is.EqualTo(expectedVelocity));
+        }
     }
 }
