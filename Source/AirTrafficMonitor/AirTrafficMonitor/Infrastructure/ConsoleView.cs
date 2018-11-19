30baa46a0bddd89e49927d54a2681cb393f0070a
Oscar Pradel <oscar.adrien.ohrt.pradel@post.au.dk>
2018-11-19 15:27:34 +0100

    Mindre ændring i ConsoleView formattering

--- Source/AirTrafficMonitor/AirTrafficMonitor/Infrastructure/ConsoleView.cs
+++ Source/AirTrafficMonitor/AirTrafficMonitor/Infrastructure/ConsoleView.cs
@@ -17,7 +17,7 @@ namespace AirTrafficMonitor.Infrastructure
             string tag1 = eventArgs.Tag1;
             string tag2 = eventArgs.Tag2;
             DateTime time = eventArgs.TimeStamp;
-            Console.WriteLine("Warning, two planes are currentl;y on collision course! " +
+            Console.WriteLine("Warning, two planes are currently on collision course! " +
                               "\n Plane Tag: {0} and plane Tag: {1}\n Current time: {2}", tag1, tag2, time);
         }
         
