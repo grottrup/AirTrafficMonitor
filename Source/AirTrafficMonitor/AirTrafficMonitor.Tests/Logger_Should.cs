7a5ca65afaceb6ec0c5084a8ce6a37e707074193
Oscar Pradel <oscar.adrien.ohrt.pradel@post.au.dk>
2018-11-19 11:37:38 +0100

    Oprettet test for Logger samt funktionelle ændringer i Logger samt ILogger - Ufuldendt!

---
+++ Source/AirTrafficMonitor/AirTrafficMonitor.Tests/Logger_Should.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using AirTrafficMonitor.Domain;
+using AirTrafficMonitor.Infrastructure;
+using AirTrafficMonitor.Utilities;
+using NUnit.Framework;
+
+
+namespace AirTrafficMonitor.Tests
+{
+    [TestFixture]
+    public class Logger_Should
+    {
+        private Logger _uut;
+
+
+        [SetUp]
+        public void Setup()
+        {
+            _uut = new Logger();
+        }
+        
+        [TestCase("AA123","BB123", "2018-08-11", "Warning, two planes are currently on collision course! " +
+                                                 "\n Plane Tag: AA123 and plane Tag: BB123\n Current time: 11-08-2018 00:00:00\r\n")]
+        [TestCase("CC123", "DD123", "2018-09-11", "Warning, two planes are currently on collision course! " +
+                                                  "\n Plane Tag: CC123 and plane Tag: DD123\n Current time: 11-09-2018 00:00:00\r\n")]
+        public void 
+    }
+}
--- Source/AirTrafficMonitor/AirTrafficMonitor/Infrastructure/Logger.cs
+++ Source/AirTrafficMonitor/AirTrafficMonitor/Infrastructure/Logger.cs
@@ -6,12 +6,16 @@ namespace AirTrafficMonitor.Infrastructure
 {
     public class Logger : ILogger
     {
-        public void DataLog(object test, FlightInCollision eventArgs)
+        public void DataLog(FlightInCollision eventArgs)
         {
+            string tag1 = eventArgs.Tag1;
+            string tag2 = eventArgs.Tag2;
+            DateTime time = eventArgs.TimeStamp;
+            
+            
             File.WriteAllText(AppDomain.CurrentDomain.BaseDirectory + "DataLog.txt",
                 "Warning, two planes are currently on collision course! " +
-                "\n Plane Tag: " + eventArgs.Tag1 + " and plane Tag: " + eventArgs.Tag2 + "\n Current time: " +
-                eventArgs.TimeStamp);
+                "\n Plane Tag: {0} and plane Tag: {1}\n Current time: {2}",tag1,tag2,time);
         }
     }
 }
