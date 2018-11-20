839bb199a6de778624038644b7df7cbb5ae13fd2
grottrup <grottrup@protonmail.com>
2018-11-20 11:19:50 +0100

    Fix merge marker

--- Source/AirTrafficMonitor/AirTrafficMonitor.ConsoleApp/Program.cs
+++ Source/AirTrafficMonitor/AirTrafficMonitor.ConsoleApp/Program.cs
@@ -9,9 +9,6 @@ using AirTrafficMonitor.Domain;
 using AirTrafficMonitor.Infrastructure;
 using AirTrafficMonitor.Utilities;
 using TransponderReceiver;
-=========
-using AirTrafficMonitor.Utilities;
->>>>>>>>> Temporary merge branch 2
 
 namespace AirTrafficMonitor.ConsoleApp
 {
