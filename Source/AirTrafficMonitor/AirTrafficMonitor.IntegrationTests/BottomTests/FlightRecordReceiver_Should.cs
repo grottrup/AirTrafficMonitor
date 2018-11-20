73e3db55ebe9eaf1f3b4845990d4036701eb1d6f
Mike <Mike-mj@hotmail.com>
2018-11-20 10:28:56 +0100

    Merge with FlightRecordReceiver_Should

---
+++ Source/AirTrafficMonitor/AirTrafficMonitor.IntegrationTests/BottomTests/FlightRecordReceiver_Should.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AirTrafficMonitor.AntiCorruptionLayer;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace AirTrafficMonitor.IntegrationTests
+{
+    [TestFixture]
+    public class FlightRecordReceiver_Should
+    {
+        private FlightRecordReceiver _sut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var subSystem1 = new FlightRecordFactory();
+            _sut = new FlightRecordReceiver(subSystem1);
+        }
+
+        [Test]
+        public void test1()
+        {
+
+        }
+    }
+}
---
+++ Source/AirTrafficMonitor/AirTrafficMonitor.IntegrationTests/TopTests/FlightObserver_Integrating_FlightRecordReceiver_Should.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AirTrafficMonitor.AntiCorruptionLayer;
+using AirTrafficMonitor.Domain;
+using AirTrafficMonitor.Infrastructure;
+using NSubstitute;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using TransponderReceiver;
+
+namespace AirTrafficMonitor.IntegrationTests.TopTests
+{
+    [TestFixture]
+    public class FlightObserver_Integrating_FlightRecordReceiver_Should
+    {
+        private FlightRecordReceiver _ssut_flightRecordReceiver;
+        private FlightObserver _sut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var fakeTransponder = Substitute.For<ITransponderReceiver>();
+
+            var airspace = new Airspace();
+            var factory = new FlightRecordFactory();
+            var fakeView = Substitute.For<IView>();
+            var fakeSeperation = Substitute.For<ISeperationHandler>();
+     
+            var fakeLogger = Substitute.For<Infrastructure.ILogger>();
+            var fakeMonitoredAirspace = Substitute.For<Airspace>();
+
+            _ssut_flightRecordReceiver = new FlightRecordReceiver(fakeTransponder, factory);
+            _sut = new FlightObserver(fakeMonitoredAirspace, _ssut_flightRecordReceiver, fakeView, fakeSeperation, fakeLogger);
+        }
+
+        [Test]
+        public void TriggerOnEventRaised()
+        {
+
+        }
+    }
+}
