e71979e75e202bba1643da62bdc9365b7ed3a9de
Oscar Pradel <oscar.adrien.ohrt.pradel@post.au.dk>
2018-11-21 12:38:30 +0100

    Final Logger + ConsoleView test. Still needs to change culture somehow.
    https://docs.microsoft.com/en-us/dotnet/api/system.globalization.cultureinfo.defaultthreadcurrentculture?view=netframework-4.7.2 maybe?

--- Source/AirTrafficMonitor/AirTrafficMonitor/Infrastructure/IView.cs
+++ Source/AirTrafficMonitor/AirTrafficMonitor/Infrastructure/IView.cs
@@ -8,7 +8,7 @@ namespace AirTrafficMonitor.Infrastructure
 {
     public interface IView
     {
-        void Render(FlightTrack track);
+        void Render(Tuple<FlightTrack> track);
         void RenderCollision(Tuple<FlightTrack, FlightTrack> flightsInCollision);
         void RenderWithGreenTillTimerEnds(string renderstr);
         void RenderWithRedTillTimerEnds(string renderstr);
