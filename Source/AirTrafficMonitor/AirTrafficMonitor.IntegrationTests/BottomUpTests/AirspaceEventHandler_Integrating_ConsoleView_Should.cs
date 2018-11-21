279183039484d14645b8da06c3c1832c5e3e7f3a
Mike <Mike-mj@hotmail.com>
2018-11-21 19:37:54 +0100

    Started with integration tests

--- Source/AirTrafficMonitor/AirTrafficMonitor.IntegrationTests/BottomUpTests/AirspaceEventHandler_Integrating_ConsoleView_Should.cs
+++ Source/AirTrafficMonitor/AirTrafficMonitor.IntegrationTests/BottomUpTests/AirspaceEventHandler_Integrating_ConsoleView_Should.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AirTrafficMonitor.Domain;
+using AirTrafficMonitor.Infrastructure;
+using NSubstitute;
 using NUnit.Framework;
 using NUnit.Framework.Internal;
 
@@ -11,5 +14,35 @@ namespace AirTrafficMonitor.IntegrationTests.BottomUpTests
     [TestFixture]
     public class AirspaceEventHandler_Integrating_ConsoleView_Should
     {
+        private AirspaceEventHandler _airspaceEventHandler;
+        private IView _View;
+        private IFlightObserver _fakeFlightObserver;
+        private IFlightTrack _fakeFlightTrack;
+        private List<IFlightTrack> _fakeFlightTracks;
+        [SetUp]
+        public void Setup()
+        {
+            _fakeFlightTracks = Substitute.For<List<IFlightTrack>>();
+            _fakeFlightTrack = Substitute.For<IFlightTrack>();
+            _fakeFlightObserver = Substitute.For<IFlightObserver>();
+            _View = new ConsoleView();
+            _airspaceEventHandler = new AirspaceEventHandler(_fakeFlightObserver, _View);
+        }
+
+        [Test]
+        public void AirspaceEvent_View_Recived()
+        {
+
+            _fakeFlightTrack = new FlightTrack("BB123")
+            {
+                Position = new Position(11000, 910000, 5000),
+                LatestTime = DateTime.MinValue
+            };
+
+            _fakeFlightTracks = new List<IFlightTrack>();
+            _fakeFlightObserver.LeftAirspace += Raise.EventWith(_fakeFlightObserver, new FlightTrackEventArgs(_fakeFlightTrack));
+
+           
+        }
     }
 }
