66837bb5352e6cb0b67b99eb8740bb1ae9486a92
grottrup <grottrup@protonmail.com>
2018-11-21 15:20:21 +0100

    Cleanup airspace event handler

--- Source/AirTrafficMonitor/AirTrafficMonitor/AirspaceEventHandler.cs
+++ Source/AirTrafficMonitor/AirTrafficMonitor/AirspaceEventHandler.cs
@@ -1,41 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Timers;
-using AirTrafficMonitor.AntiCorruptionLayer;
-using AirTrafficMonitor.Domain;
-using AirTrafficMonitor.Infrastructure;
-using AirTrafficMonitor.Utilities;
-using Timer = System.Threading.Timer;
-
+﻿using AirTrafficMonitor.Infrastructure;
 
 namespace AirTrafficMonitor
 {
     public class AirspaceEventHandler
     {
-        private readonly List<FlightTrack> _track;
-        private readonly IFlightObserver _observerEvents;
+        private readonly IFlightObserver _flightInAirspaceSubject;
         private IView _view;
 
-        public AirspaceEventHandler(IFlightObserver observerEvents, IView view) 
+        public AirspaceEventHandler(IFlightObserver flightInAirspaceSubject, IView view) 
         {
-            _observerEvents = observerEvents;
+            _flightInAirspaceSubject = flightInAirspaceSubject;
             _view = view;
-            _observerEvents.EnteredAirspace += EnterAirspaceEvent;
-            _observerEvents.LeftAirspace += LeftAirspaceEvent;
-            _track = new List<FlightTrack>();   
+            _flightInAirspaceSubject.EnteredAirspace += EnterAirspaceEvent;
+            _flightInAirspaceSubject.LeftAirspace += LeftAirspaceEvent;
         }
 
-        public void EnterAirspaceEvent(object sender,FlightTrackEventArgs e)
+        private void EnterAirspaceEvent(object sender,FlightTrackEventArgs e)
         {
                 var flightUpdate = e.FlightTrack;
                 _view.RenderWithRedTillTimerEnds("Flight "+flightUpdate.Tag+" entered airspace at"+flightUpdate.LatestTime+"");
               
         }
 
-        public void LeftAirspaceEvent(object sender, FlightTrackEventArgs e)
+        private void LeftAirspaceEvent(object sender, FlightTrackEventArgs e)
         {
             var flightUpdate = e.FlightTrack;
             _view.RenderWithGreenTillTimerEnds("Flight "+flightUpdate.Tag+" left airspace at"+flightUpdate.LatestTime+"");
