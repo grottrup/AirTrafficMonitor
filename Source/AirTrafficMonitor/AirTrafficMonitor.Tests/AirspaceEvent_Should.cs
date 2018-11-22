4592f5a532613e35a0630bcab4caf63b5f42c5e5
Michelle True Sørensen <farcait@gmail.com>
2018-11-22 22:21:26 +0100

    Creates test for AirspaceEventHandler.DangerOfProximityEvent

--- Source/AirTrafficMonitor/AirTrafficMonitor.Tests/AirspaceEvent_Should.cs
+++ Source/AirTrafficMonitor/AirTrafficMonitor.Tests/AirspaceEvent_Should.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using AirTrafficMonitor.Domain;
 using AirTrafficMonitor.Infrastructure;
 using AirTrafficMonitor.Utilities;
@@ -14,18 +16,41 @@ namespace AirTrafficMonitor.Tests
         private IView _fakeView;
         private IFlightObserver _fakeFlightObserver;
         private AirspaceEventHandler _uut;
-
+        private ISeperationHandler _fakeSeperation;
 
         [SetUp]
         public void SetUp()
         {
             _fakeTimer = Substitute.For<ITimer>();
             _fakeView = Substitute.For<IView>();
+            _fakeSeperation = Substitute.For<ISeperationHandler>();
             var fakeLogger = Substitute.For<ILogger>();
-            var fakeSeperation = Substitute.For<ISeperationHandler>();
 
             _fakeFlightObserver = Substitute.For<IFlightObserver>();
-            _uut = new AirspaceEventHandler(_fakeFlightObserver, _fakeView, fakeLogger, fakeSeperation);
+            _uut = new AirspaceEventHandler(_fakeFlightObserver, _fakeView, fakeLogger, _fakeSeperation);
+        }
+
+
+        [TestCase("CC123", 2018, 1, 1, "CC456", 2018, 1, 1)]
+        public void Trigger_Warning(string tag, int year, int month, int day, string tag2, int year2, int month2, int day2)
+        {
+            // Act
+            var fakeFlightTrack = Substitute.For<IFlightTrack>();
+            fakeFlightTrack.Tag.Returns(tag);
+            fakeFlightTrack.LatestTime.Returns(new DateTime(year, month, day));
+
+            var fakeFlightTrack2 = Substitute.For<IFlightTrack>();
+            fakeFlightTrack.Tag.Returns(tag2);
+            fakeFlightTrack.LatestTime.Returns(new DateTime(year2, month2, day2));
+
+            Tuple<IFlightTrack, IFlightTrack> fakeTracks = new Tuple<IFlightTrack, IFlightTrack>(fakeFlightTrack, fakeFlightTrack2);
+
+            _fakeSeperation.FlightsInProximity += Raise.EventWith(_fakeSeperation, new FlightInProximityEventArgs(fakeTracks));
+
+            var expectedResult = $"Danger! Proximity of {fakeTracks.Item1.Tag} and {fakeTracks.Item2.Tag}";
+
+            //Assert
+            _fakeView.Received().AddToRenderWithColor(expectedResult, ConsoleColor.Red);
         }
 
         [TestCase("CC123", 2018, 1, 1)]
