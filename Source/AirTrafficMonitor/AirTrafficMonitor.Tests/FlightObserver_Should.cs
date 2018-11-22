a76338d98d63f990c4f2f6dd20519c6c3c1b8792
grottrup <grottrup@protonmail.com>
2018-11-22 18:16:15 +0100

    Update more test

--- Source/AirTrafficMonitor/AirTrafficMonitor.Tests/FlightObserver_Should.cs
+++ Source/AirTrafficMonitor/AirTrafficMonitor.Tests/FlightObserver_Should.cs
@@ -65,7 +65,7 @@ namespace AirTrafficMonitor.Tests
             _fakeFlight.FlightRecordReceived += Raise.EventWith(_fakeFlight, new FlightRecordEventArgs(record));
 
             // Assert
-            _fakeSeperation.Received().DetectCollision(Arg.Any<Tuple<IFlightTrack, IFlightTrack>>());
+            _fakeSeperation.Received().DetectCollision(Arg.Any<ICollection<IFlightTrack>>());
         }
 
         [Test]
